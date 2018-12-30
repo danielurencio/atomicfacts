ddf340ece8fec7d42432de1903c3bf44a9f3cba9
danielurencio <danielurencio@gmail.com>
2018-12-29 21:10:34 -0600

    d

--- MarketEnv.py
+++ MarketEnv.py
@@ -1,107 +1,205 @@
 import pandas as pd
 import numpy as np
-from Classes import Instrument
-from sklearn.preprocessing import MinMaxScaler, StandardScaler
+from sklearn.preprocessing import StandardScaler
+
+df = pd.read_csv('fx_daily_EUR_USD.csv',parse_dates=['timestamp'],index_col='timestamp')
+df.sort_index(inplace=True)
+df['position'] = 0
 
 
-class MarketEnv(Instrument):
+class MarketEnv(object):
+    
+    def __init__(self, df, lag, standardize=True, hold=True, test_set=None):
+    # Configuration:
+        if not len(df) or not lag:
+            raise ValueError('No DataFrame or lag has been determined.')
+            
+        self.df = df.copy()
+        self.cols = self.df.columns.tolist()[:-1]
+        self.lag = 8
+        self.standardize = standardize
+        self.hold = hold
+        self.laggedFeatures = self.generate_LaggedDataset()
+        self.step_ix = 0
+        self.price = None
+        self.last_step_ix = (len(self.df) - self.lag)
 
-    def __init__(self,st='close',instrument='EUR_USD',lags=(5,22)):
-        self.step = 0
 
-        self.setData(instrument)
+    def data_AsMovingWindow(self):
+    # Arrange each feature as a lagged moving window:
+        df = self.df
+        cols = self.cols
+        arr = []
 
-        for i,d in enumerate(lags):
-            if i == 0:
-                self.addFeatures(st,d)
-                self.data_ = self.df.copy()
+        for i in range(len(df)):
+            fn = lambda x: df[x].values[i:i+self.lag].tolist()
+            vals = list(map(fn,cols))
+            arr.append(vals)
 
+        # Remove observations that don't have a length equal to the value specified by the variable 'lag':
+        arr = arr[:-(self.lag - 1)]
+        
+        return arr
+
+    
+    def generate_LaggedDataset(self):
+        
+        arr = self.data_AsMovingWindow()
+        self.scaler = StandardScaler()
+        
+        def standardizeValues(col):
+            '''fn: isolate each set of observations and arrange it as a moving window'''
+            index = col[0]
+            column_name = col[1]
+
+            fn = lambda x:x[index]
+            feature_vector = map(fn,arr)
+
+            cond = column_name not in ['position','balance']
+
+            if self.standardize and cond:
+                data = list(feature_vector)
+                self.scaler.fit(data)
+                result = self.scaler.transform(data, copy=True)
+                #result = self.scaler.fit_transform(list(feature_vector)).tolist()
+                
             else:
-                self.addFeatures(st,d)
-                self.data_ = self.data_.join(self.df.iloc[:,4:]).dropna()
+                result = list(feature_vector)
 
-        self.data_['position'] = 0
+            return result
 
-
-    def aa(self):
-        data = self.data_.copy()
-
-        lag = 22
-
-        def arrangeData(a,back):
-            b = [ a[i:i+back] for i,d in enumerate(a) ]
-            b = list(filter(lambda x:len(x) == lag,b) )
-            return b
-
-        def scale_(x):
-            aa = arrangeData(data[x].tolist(),lag)
-            bb = np.array(aa)
-            if x != 'position':
-                scaler = MinMaxScaler()
-                scaler.fit(bb)
-                bb = scaler.transform(bb)
-            return bb
-
-        def accomodateAsSequences(arr):
-            cont = [ [] for d in range(arr[0].shape[0]) ]
-
-            for i in arr:
-                for j,d in enumerate(i):
-                    cont[j].append(d.tolist())
-
-            cc_ = np.array(list(map(lambda x:list(zip(*x)),cont)))
-            return cc_
-
-        cols = data.columns.tolist()
-        gg = list(map(scale_,cols))
-        gg = accomodateAsSequences(gg)
-        return gg
+        laggedFeatures = list(map(standardizeValues,enumerate(self.cols)))
+        
+        return laggedFeatures
 
 
-    def reset(self):
-        self.step = 0
-        return self.step_(1)[0]
 
-    def step_(self,action):
-        row = self.data_.iloc[self.step + 0 + 21]
-        currentPrice = row['close']
-        nextPrice = self.data_.iloc[self.step + 1 + 21]
-        ix_ = self.step + 1 + 21
-        row_andAhead = self.data_.iloc[ix_:].index
-        position = row['position']
+    def add_TradePosition(self,nth):
+        #
+        # 'features' variable:
+        # Stores a set with the nth position of all available features (as a list/array):
+        #   e.g: { [lagged_feature_1], [lagged_feature_2], ..., [lagged_feature_n] }
+        #
+        features = map(lambda x:self.laggedFeatures[x][nth],range(len(self.laggedFeatures)))
+        
+        # The next line allows to add dinamically changed features on the fly:
+        concatenation = list(features) + [ self.df.iloc[nth:nth+self.lag].position.tolist() ]
+        
+        # Combine all features in one single vector:
+        zippedFeatures = list(zip(*concatenation))
+        
+        return np.array(zippedFeatures)
+    
+    
+    def actionLogic(self,action):
+        #
+        # This index let's us know what are the values of the lastest step such as:
+        #   * The closing price.
+        #   * Whether we had a short or a long position in order to:
+        #       - Close the position if we open an opposite transaction.
+        #       - Hold the position if we pass or execute the same transaction.
+        #
+        self.index = (self.step_ix + self.lag) - 1         # This is where we calculate this index.
+        self.latest_step = self.df.iloc[self.index].copy() # This is how we use it: getting the latest step,
+                                                           #  and its attriutes (closing price, kind of transaction)
+            
+        def positionType_givenAction(condition,positionType):
+            
+            if self.hold:
+                if self.latest_step.position:
+                    # If we have a position and an opposite one is afterward placed:
+                    if condition:#self.latest_step.position > 0:
+                        # Get rid of position:
+                        next_position = 0
+                    # Else, if the position is the same as the one already held:
+                    else:
+                        # Keep this position.
+                        next_position = positionType
+                else:
+                    next_position = positionType
+                    
+            else:
+                next_position = positionType
+                
+            return next_position
+        
+        
+            
+        if action == 0:
+            # Sell:
+            # If we SELL and the PREVIOUS open position is POSITIVE, handle it:
+            next_position = positionType_givenAction(self.latest_step.position > 0, -1)
 
-        if(action == 0):
-            if position == 1:
-                reward = self.price - currentPrice
-                self.price = currentPrice
+        elif action == 1:
+            # Buy:
+            # If we BUY and the PREVIOUS open position is NEGATIVE, handle it:
+            next_position = positionType_givenAction(self.latest_step.position < 0, 1)
+                
+        elif action == 2:
+            # Pass:
+            next_position = self.latest_step.position
+        
+        # All steps forward are set to the value specified by 'next_position'
+        self.df.loc[self.df.iloc[self.index:].index,'position'] = next_position
 
-            elif position == 0:
-                self.price = row['close']
-                reward = 0
 
-            else: # position == -1
-                reward = 0
+                
+    def calculateReward(self):
 
-            self.data_.loc[row_andAhead,'position'] = -1
+        lastTwo = self.df.iloc[self.index-1:self.index+1]
+        lastPositions = lastTwo.position.tolist()
+        change = [lastPositions[0]] * len(lastTwo) != lastPositions
 
-        if(action == 2):
+        if change:
+            # If we are either opening a position for the firt time or closing one without re-opening another:
+            latestPrice = lastTwo.close[-1]
+            
+            if 0 in lastPositions:
+                # If we previously did not have an open transaction and now we do:
+                if lastPositions[-1] and not lastPositions[-2]:
+                    self.price = lastTwo.close[-1]
+                    reward = 0
+
+                # If we get rid of a position:
+                else:
+                    reward = latestPrice - self.price if lastPositions[-1] == 1 else self.price - latestPrice
+                    self.price = None
+            
+            # If we transition from one kind of position to another without the 'hold' config:
+            else:
+                reward = latestPrice - self.price if lastPositions[-1] == 1 else self.price - latestPrice
+                self.price = latestPrice
+
+        else:
             reward = 0
+        
+        return reward
 
-        if(action == 1):
-            if position == -1:
-                reward = currentPrice - self.price
-                self.price = currentPrice
+    
+    def reset(self):
+        state = self.add_TradePosition(0)
+        self.step_ix = 1
+        return state
+    
+        
+    def step(self,action):
+        
+        if self.step_ix == 0:
+            raise ValueError('You need to run reset first.')
+        
+        self.actionLogic(action)
+        
+        reward = self.calculateReward()
+        state = self.add_TradePosition(self.step_ix)
+                
+        if self.last_step_ix > self.step_ix:
+            done = False
+            self.step_ix += 1
+            
+        else:
+            done = True
+            self.step_ix = self.last_step_ix
 
-            elif position == 0:
-                self.price = currentPrice
-                reward = 0
-
-            else: # positino == 1
-                reward = 0
-
-            self.data_.loc[row_andAhead,'position'] = 1
-
-        finish = None if self.step < len(self.data_)-1 else True
-        self.step += 1
-        state = self.aa()[self.step]
-        return [state,reward,finish]
+        result = (state,reward,done)
+        
+        return result
--- vanilla_pg.py
+++ vanilla_pg.py
@@ -1,13 +1,20 @@
 import tensorflow as tf
 import tensorflow.contrib.slim as slim
+import pandas as pd
 import numpy as np
 import gym
 import matplotlib.pyplot as plt
 from MarketEnv import MarketEnv
 from Classes import Instrument
 
+df = pd.read_csv('fx_daily_EUR_USD.csv',parse_dates=['timestamp'],index_col='timestamp')
+df.sort_index(inplace=True)
+df['position'] = 0
+
+seq_size = 8
+
 #env = gym.make('CartPole-v0')
-env = MarketEnv()
+env = MarketEnv(df,seq_size)
 
 
 
@@ -16,9 +23,11 @@ def discount_rewards(r):
     """ take 1D float array of rewards and compute discounted reward """
     discounted_r = np.zeros_like(r)
     running_add = 0
-    for t in reversed(xrange(0, r.size)):
+
+    for t in reversed(range(0, r.size)):
         running_add = running_add * gamma + r[t]
         discounted_r[t] = running_add
+
     return discounted_r
 
 
@@ -27,7 +36,7 @@ class agent():
     def __init__(self, lr, s_size,a_size,h_size,stacks=1):
         #These lines established the feed-forward part of the network. The agent takes a state and produces an action.
         _seqlens = tf.placeholder(tf.float32,shape=[s_size])
-        self.state_in= tf.placeholder(shape=[None,s_size,15],dtype=tf.float32)
+        self.state_in= tf.placeholder(shape=[None,s_size,5],dtype=tf.float32)
 
         def lstm_cell_():
           return tf.contrib.rnn.BasicLSTMCell(h_size)
@@ -56,6 +65,7 @@ class agent():
 
         tvars = tf.trainable_variables()
         self.gradient_holders = []
+        
         for idx,var in enumerate(tvars):
             placeholder = tf.placeholder(tf.float32,name=str(idx)+'_holder')
             self.gradient_holders.append(placeholder)
@@ -71,7 +81,7 @@ class agent():
 
 tf.reset_default_graph() #Clear the Tensorflow graph.
 
-myAgent = agent(lr=1e-2,s_size=22,a_size=3,h_size=32) #Load the agent.
+myAgent = agent(lr=1e-2,s_size=seq_size,a_size=3,h_size=32) #Load the agent.
 
 total_episodes = 5000 #Set total number of episodes to train agent on.
 max_ep = 999
@@ -94,38 +104,50 @@ with tf.Session() as sess:
         s = env.reset()
         running_reward = 0
         ep_history = []
-        for j in range(max_ep):
+
+        while True:
+#        for j in range(max_ep):
             #Probabilistically pick an action given our network outputs.
             a_dist = sess.run(myAgent.output,feed_dict={myAgent.state_in:[s]})
             a = np.random.choice(a_dist[0],p=a_dist[0])
             a = np.argmax(a_dist == a)
-
-            s1,r,d = env.step_(a) #Get our reward for taking an action given a bandit.
+            
+            s1,r,d = env.step(a) #Get our reward for taking an action given a bandit.
             ep_history.append([s,a,r,s1])
             s = s1
             running_reward += r
+
             if d == True:
                 #Update the network.
                 ep_history = np.array(ep_history)
                 ep_history[:,2] = discount_rewards(ep_history[:,2])
+
                 feed_dict={myAgent.reward_holder:ep_history[:,2],
-                        myAgent.action_holder:ep_history[:,1],myAgent.state_in:np.vstack(ep_history[:,0])}
+                        myAgent.action_holder:ep_history[:,1],
+                        myAgent.state_in:np.array(list(map(lambda x: np.array(x,dtype=np.float32), ep_history[:,0])))
+                        #np.vstack(ep_history[:,0])
+                        }
+
                 grads = sess.run(myAgent.gradients, feed_dict=feed_dict)
+
                 for idx,grad in enumerate(grads):
                     gradBuffer[idx] += grad
 
                 if i % update_frequency == 0 and i != 0:
                     feed_dict= dictionary = dict(zip(myAgent.gradient_holders, gradBuffer))
+
                     _ = sess.run(myAgent.update_batch, feed_dict=feed_dict)
+
                     for ix,grad in enumerate(gradBuffer):
                         gradBuffer[ix] = grad * 0
 
                 total_reward.append(running_reward)
-                total_lenght.append(j)
+#                total_lenght.append(j)
                 break
 
 
             #Update our running tally of scores.
         if i % 100 == 0:
             print(np.mean(total_reward[-100:]))
+
         i += 1
